cba9cad437d492d425922175f0fa9a437eba9bea
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-10 15:33:04 -0400

    structure fix

--- nft_frontend/src/App.js
+++ nft_frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import ChatBox from "./pages/ChatBox/ChatBox";
+import CreateNft from "./pages/CreateNft/CreateNft";
+import Settings from "./pages/Settings/Settings";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<CreateNft />} />
+        <Route path="/chatbox" element={<ChatBox />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
