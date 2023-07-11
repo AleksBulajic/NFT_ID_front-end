9173aa6f0f650fca0e389f0707151d1dbaf7a882
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-11 09:53:04 -0400

    chatbox,about,create

--- nft_frontend/src/pages/Settings/Settings.jsx
+++ nft_frontend/src/pages/Settings/Settings.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import './settings.css'
-import '../../App.css'
+// import React from 'react'
+// import './settings.css'
+// import '../../App.css'
 
-const Settings= () => {
+// const Settings= () => {
+//   return (
+//     <div className = "page-container">
+//         <h1 className = "page-title">Settings </h1>
+//     </div>
+//   )
+// }
+
+// export default Settings;
+
+// Settings.js
+import React, { useState } from 'react';
+import AboutModal from './AboutModal';
+
+const Settings = () => {
+  const [isAboutModalOpen, setAboutModalOpen] = useState(false);
+
+  const handleAboutClick = () => {
+    setAboutModalOpen(true);
+  };
+
+  const handleAboutModalClose = () => {
+    setAboutModalOpen(false);
+  };
+
   return (
-    <div className = "page-container">
-        <h1 className = "page-title">Settings </h1>
+    <div>
+      <button onClick={handleAboutClick} style={{ background: 'none', border: 'none', color: '#ffffff' }}>
+        About
+      </button>
+      <AboutModal isOpen={isAboutModalOpen} onRequestClose={handleAboutModalClose} />
     </div>
-  )
-}
+  );
+};
 
 export default Settings;
-
