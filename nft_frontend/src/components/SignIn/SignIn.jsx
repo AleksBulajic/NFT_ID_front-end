675cf0faf9da94c833932209d58a3365e1ec0b4b
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-12 11:36:42 -0400

    home fix

--- nft_frontend/src/components/SignIn/SignIn.jsx
+++ nft_frontend/src/components/SignIn/SignIn.jsx
@@ -1,48 +1,48 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-// const SignIn = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-//   const handleSignIn = async (e) => {
-//     e.preventDefault();
+  const handleSignIn = async (e) => {
+    e.preventDefault();
 
-//     try {
-//       const response = await axios.post('/api/signin/', { email, password });
+    try {
+      const response = await axios.post('/api/signin/', { email, password });
 
-//       // Handle successful sign-in response here
-//       console.log(response.data);
+      // Handle successful sign-in response here
+      console.log(response.data);
 
-//       // Redirect the user to a different page or perform other actions
-//     } catch (error) {
-//       // Handle sign-in error here
-//       console.error('Sign-in failed:', error);
-//     }
-//   };
+      // Redirect the user to a different page or perform other actions
+    } catch (error) {
+      // Handle sign-in error here
+      console.error('Sign-in failed:', error);
+    }
+  };
 
-//   return (
-//     <div>
-//       <h2>Sign In</h2>
-//       <form onSubmit={handleSignIn}>
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//           required
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//           required
-//         />
-//         <button type="submit">Sign In</button>
-//       </form>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSignIn}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+};
 
-// export default SignIn;
+export default SignIn;
--- nft_frontend/src/pages/Home/Home.jsx
+++ nft_frontend/src/pages/Home/Home.jsx
@@ -1,48 +1,15 @@
-
-import React, { useState } from 'react';
-import SignUp from '../SignUp/SignUp';
-import { useLocation } from 'react-router-dom'
-
 import React from 'react'
 import IdCard from '../../components/IdCard/IdCard'
-
 import './home.css'
+import '../../App.css'
 
-const SignIn = () => {
-  const [showSignUp, setShowSignUp] = useState(false);
-
-  const handleSignUpClick = (e) => {
-    e.preventDefault();
-    setShowSignUp(!showSignUp);
-  };
-
+const Home= () => {
   return (
-    <div className="home-container">
-      {showSignUp ? (
-        <SignUp handleSignUpClick={handleSignUpClick} />
-      ) : (
-        <>
-          <h2>Sign In</h2>
-          <form className="home-form">
-            <input type="text" placeholder="Username" required />
-            <input type="password" placeholder="Password" required />
-            <button type="submit">Sign In</button>
-          </form>
-          <span className="goTo">
-            Don't have an account? Click{' '}
-            <a href="/signup" onClick={handleSignUpClick}>
-              here
-            </a>{' '}
-            to sign up instead.
-          </span>
-        </>
-      )}
     <div className = "page-container">
         <h1 className = "page-title">Hello and Welcome!</h1>
         <IdCard />
-
     </div>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default Home;
