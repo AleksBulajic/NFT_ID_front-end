4b95e94d198f3f30b26080285f04576c82c713dd
genpai99 <darioussanchez@gmail.com>
2023-07-12 10:11:55 -0400

    Settings/Landing page update

---
+++ nft_frontend/src/pages/LandingScreen/LandingScreen.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import SignUp from '../SignUp/SignUp';
+import { useLocation } from 'react-router-dom'
+import './landingscreen.css'
+
+const SignIn = () => {
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  const handleSignUpClick = (e) => {
+    e.preventDefault();
+    setShowSignUp(!showSignUp);
+  };
+
+  return (
+    <div className="home-container">
+      {showSignUp ? (
+        <SignUp handleSignUpClick={handleSignUpClick} />
+      ) : (
+        <>
+          <h2>Sign In</h2>
+          <form className="home-form">
+            <input type="text" placeholder="Username" required />
+            <input type="password" placeholder="Password" required />
+            <button type="submit">Sign In</button>
+          </form>
+          <span className="goTo">
+            Don't have an account? Click{' '}
+            <a href="/signup" onClick={handleSignUpClick}>
+              here
+            </a>{' '}
+            to sign up instead.
+          </span>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default SignIn;
