4f2358cad7c58321f0d02d5b10c239ebb5dd12c2
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-13 10:22:31 -0400

    storing for later

--- nft_frontend/src/components/IdCard/IdCard.jsx
+++ nft_frontend/src/components/IdCard/IdCard.jsx
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
-import './idcard.css';  
+import './idcard.css';
 
 const IdCard = () => {
   const user = {
     name: 'Devin Shrek',
-    image: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2022/08/Retrofuturism-in-design.jpg?auto=format&q=60&w=1860&h=1860&fit=crop&crop=faces',
+    image:
+      'https://99designs-blog.imgix.net/blog/wp-content/uploads/2022/08/Retrofuturism-in-design.jpg?auto=format&q=60&w=1860&h=1860&fit=crop&crop=faces',
     detail1: 'Wallet Id Number',
     detail2: 'NY, NY',
-    detail3: '72 inches'
+    detail3: '72 inches',
   };
 
-  const [props, set] = useSpring(() => ({
-    scale: 1,
-  }));
+  const [scale, setScale] = useState(1);
+  const [isDetailVisible, setDetailVisible] = useState(false);
+
+  const scaleProps = useSpring({ transform: `scale(${scale})` });
+
+  const detailProps1 = useSpring({
+    transform: isDetailVisible ? 'translate(100px, 50px)' : 'translate(0px, 0px)',
+    config: { tension: 210, friction: 20 },
+  });
+
+  const detailProps2 = useSpring({
+    transform: isDetailVisible ? 'translate(-50px, 100px)' : 'translate(0px, 0px)',
+    config: { tension: 210, friction: 20 },
+  });
+
+  const detailProps3 = useSpring({
+    transform: isDetailVisible ? 'translate(50px, -100px)' : 'translate(0px, 0px)',
+    config: { tension: 210, friction: 20 },
+  });
+
+  const handleCardClick = () => {
+    setDetailVisible(!isDetailVisible);
+  };
 
   return (
-    <animated.div 
-      className="idCard" 
-      style={props} 
-      onMouseEnter={() => set({ scale: 1.2 })} 
-      onMouseLeave={() => set({ scale: 1 })} 
-    >
-      <h2>{user.name}</h2>
-      <div className="id-card-content">
-        <img src={user.image} alt={user.name} />
-        <p>{user.detail1}</p>
-        <p>{user.detail2}</p>
-        <p>{user.detail3}</p>
-      </div>
-    </animated.div>
+    <div className="idCardContainer">
+      <animated.div
+        className="idCard"
+        style={scaleProps}
+        onMouseEnter={() => setScale(1.2)}
+        onMouseLeave={() => setScale(1)}
+        onClick={handleCardClick}
+      >
+        <h2>{user.name}</h2>
+        <div className="id-card-content">
+          <img src={user.image} alt={user.name} />
+        </div>
+      </animated.div>
+      <animated.p className={isDetailVisible ? "spread" : ""} style={detailProps1}>{user.detail1}</animated.p>
+      <animated.p className={isDetailVisible ? "spread" : ""} style={detailProps2}>{user.detail2}</animated.p>
+      <animated.p className={isDetailVisible ? "spread" : ""} style={detailProps3}>{user.detail3}</animated.p>
+    </div>
   );
 };
 
