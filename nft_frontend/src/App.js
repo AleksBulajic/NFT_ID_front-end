9637999112278181c02ac62948731d15053dc0c3
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-13 03:37:12 -0400

    simple animations

--- nft_frontend/src/App.js
+++ nft_frontend/src/App.js
@@ -1,6 +1,6 @@
-// App.jsx
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { useTransition, animated } from 'react-spring';
 import Navbar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
 import ChatBox from "./components/ChatBox/ChatBox";
@@ -9,6 +9,23 @@ import Settings from "./pages/Settings/Settings";
 import LandingScreen from "./pages/LandingScreen/LandingScreen";
 import SignUp from "./pages/SignUp/SignUp";
 import AboutModal from "./components/About/AboutModal";
+
+const AnimatedRoutes = ({ children }) => {
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+  });
+
+  return transitions((props, item) => (
+    <animated.div style={props}>
+      <Routes location={item}>
+        {children}
+      </Routes>
+    </animated.div>
+  ));
+};
 
 const App = () => {
   const [isAboutModalOpen, setAboutModalOpen] = useState(false);
@@ -25,13 +42,13 @@ const App = () => {
     <div>
       <Router>
         <Navbar onAboutOpen={handleAboutOpen} />
-        <Routes>
+        <AnimatedRoutes>
           <Route exact path="/" element={<LandingScreen />} />
           <Route path="/home" element={<Home />} />
           <Route path="/create" element={<CreateNft />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/signup" element={<SignUp />} />
-        </Routes>
+        </AnimatedRoutes>
         <AboutModal
           isOpen={isAboutModalOpen}
           onRequestClose={handleAboutClose}
