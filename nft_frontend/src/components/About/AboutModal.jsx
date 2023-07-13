24e8e05e80daaa48241a581df9055a457131dc13
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-13 10:18:29 -0400

    aboutmodal error fix

--- nft_frontend/src/components/About/AboutModal.jsx
+++ nft_frontend/src/components/About/AboutModal.jsx
@@ -1,8 +1,16 @@
-// AboutModal.js
 import React from "react";
 import Modal from "react-modal";
+import { useSpring, animated } from "react-spring";
+
+Modal.setAppElement("#root");
 
 const AboutModal = ({ isOpen, onRequestClose }) => {
+  // Define the animation config for the modal
+  const modalAnimation = useSpring({
+    opacity: isOpen ? 1 : 0,
+    transform: isOpen ? "translateY(0%)" : "translateY(-50%)",
+  });
+
   return (
     <Modal
       isOpen={isOpen}
@@ -13,18 +21,25 @@ const AboutModal = ({ isOpen, onRequestClose }) => {
           backgroundColor: "rgba(0, 0, 0, 0.5)",
           zIndex: 10000,
         },
-        content: { color: "#ffffff", backgroundColor: "#2f1c2c" },
+        content: {
+          color: "#ffffff",
+          backgroundColor: "#2f1c2c",
+          border: "none",
+          borderRadius: "8px",
+        },
       }}
     >
-      <h2 className="page-title">About NFTGems</h2>
-      <p>
-
-        NFTGems is a platform that allows you to manage your NFTs and wallet information in one place. 
-        You can upload your NFTs, view them in your personal gallery, and share them with others. 
-        Stay tuned for more features! To get started...
-
-      </p>
-      <button onClick={onRequestClose}>Close</button>
+      {/* Wrap the modal content with the animated component */}
+      <animated.div style={modalAnimation}>
+        <h2 className="page-title">About NFTGems</h2>
+        <p>
+          NFTGems is a platform that allows you to manage your NFTs and wallet
+          information in one place. You can upload your NFTs, view them in your
+          personal gallery, and share them with others. Stay tuned for more
+          features! To get started...
+        </p>
+        <button onClick={onRequestClose}>Close</button>
+      </animated.div>
     </Modal>
   );
 };
