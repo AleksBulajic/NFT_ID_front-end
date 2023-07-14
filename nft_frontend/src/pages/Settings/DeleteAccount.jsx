a6782ac36d9e3355e041eb1bfecc5cb00279d4de
Jamari02 <castrojamari@gmail.com>
2023-07-14 14:19:23 -0400

    Delete + send root

--- nft_frontend/src/pages/Settings/DeleteAccount.jsx
+++ nft_frontend/src/pages/Settings/DeleteAccount.jsx
@@ -2,15 +2,18 @@ import React, { useContext } from "react";
 import axios from "axios";
 import './settings.css';
 import { AuthContext } from "../../auth/AuthContextComponent";
+import { useNavigate } from "react-router-dom";
 
 const DeleteAccount = () => {
   const { user } = useContext(AuthContext);
-
+  const navigate = useNavigate()
   const handleDeleteAccount = async () => {
     const confirmed = window.confirm("Are you sure you want to delete your account?");
     if (confirmed) {
       // Retrieve token from local storage
       const token = localStorage.getItem('token');
+      window.alert("Account has been deleted")
+      navigate('/')
       if (!token) {
         console.error('No token found');
         return;
@@ -30,7 +33,7 @@ const DeleteAccount = () => {
         // Log the user out after successful deletion
         localStorage.removeItem('user')
         localStorage.removeItem('token');
-        window.location.href = '/login'; // replace with your login page path
+        window.location.href = '/'; // replace with your login page path
       } catch (error) {
         console.error('Deletion failed:', error.response ? error.response.data : error.message);
       }
