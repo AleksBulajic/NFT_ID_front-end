ba31ab232cb1b65c476ad28e5629ef1e6108ea2b
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-16 12:02:57 -0400

    customize theme needs fix to other components

--- nft_frontend/src/pages/Settings/CustomizeTheme.jsx
+++ nft_frontend/src/pages/Settings/CustomizeTheme.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './settings.css'
+import React, { useState } from 'react';
+import './settings.css';
 
-const CustomizeTheme= () => {
+const CustomizeTheme = ({ onThemeChange }) => {
+  const colors = ['#2f1c2c', 'blue', 'green', 'yellow', 'red', 'pink', 'orange'];
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
+  };
+
+  const handleThemeChange = () => {
+    if (selectedColor) {
+      onThemeChange(selectedColor);
+    }
+  };
+
   return (
-    <div className = "page-container">
-        <h1 className = "page-title">Customize Theme</h1>
+    <div className="page-container">
+      <h1 className="page-title">Customize Theme</h1>
+      <div className="color-options">
+        {colors.map((color, index) => (
+          <div
+            key={index}
+            className={`color-option ${selectedColor === color ? 'selected' : ''}`}
+            style={{ backgroundColor: color }}
+            onClick={() => handleColorClick(color)}
+          />
+        ))}
+      </div>
+      <button className="change-theme-button" onClick={handleThemeChange}>
+        Change Theme
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default CustomizeTheme;
