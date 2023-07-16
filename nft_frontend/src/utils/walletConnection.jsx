e99854895095b50e6340a157080d0c775ddf0d60
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-07-16 14:26:14 -0400

    walletConnection-fixed

--- nft_frontend/src/utils/walletConnection.jsx
+++ nft_frontend/src/utils/walletConnection.jsx
@@ -1,39 +1,39 @@
-export const connectWallet = async () => {
+export const ConnectWallet = async () => {
     // Metamask injects ethereum to the window object
     if (window.ethereum) {
-        try {
-          const addressArray = await window.ethereum.request({
-            method: "eth_requestAccounts",
-          });
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
   
-          const obj = {
-            status: "👆🏽 Write a message in the text-field above.",
-            address: addressArray[0],
-          };
-          
-          return obj;
-        } catch (err) {
-          return {
-            address: "",
-            status: "😥 " + err.message,
-          };
-        }
-    } else {
+        const obj = {
+          status: "👆🏽 Write a message in the text-field above.",
+          address: addressArray[0],
+        };
+  
+        return obj;
+      } catch (err) {
         return {
           address: "",
-          status: (
-            <span>
-              <p>
-                {" "}
-                🦊{" "}
-                <a target="_blank" href={`https://metamask.io/download`}>
-                  You must install Metamask, a virtual Ethereum wallet, in your
-                  browser.
-                </a>
-              </p>
-            </span>
-          )
+          status: "😥 " + err.message,
         };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
     }
   };
   
@@ -78,3 +78,4 @@ export const connectWallet = async () => {
       };
     }
   };
+  
